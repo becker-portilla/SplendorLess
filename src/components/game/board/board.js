e72917d57609b05e9d458c8b00a121317795eef7
Becker <portilla.becker@gmail.com>
2020-01-22 21:28:11 -0500

    Avance cards y board

---
+++ src/components/game/board/board.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Cards from '../../../common/cards';
+import Card from '../card/card';
+import Util from '../../../common/utils';
+
+function Board(props){
+    let cardsL1 = Cards.filter(x=> x.level == 1);
+    let cardsL2 = Cards.filter(x=> x.level == 2);
+    let cardsL3 = Cards.filter(x=> x.level == 3);
+
+    Util.shuffle(cardsL1);
+    Util.shuffle(cardsL2);
+    Util.shuffle(cardsL3);
+
+    return (
+        <div>
+            {ShowCards(cardsL1)}
+            {ShowCards(cardsL2)}
+            {ShowCards(cardsL3)}
+        </div>
+    );
+}
+
+function ShowCards(list){
+    return (<div className="cards-level">
+        {list.slice(0, 4).map(x=>{
+            return (<Card IdCard={x.id}></Card>)
+        })}
+        </div>)
+}
+
+export default Board;
--- src/components/game/card/card.js
+++ src/components/game/card/card.js
@@ -4,9 +4,27 @@ import Cards from '../../../common/cards';
 /*props: 
     idCard (int)
 */
-function Card() {
-    let card = Cards.filter(x=>x.id == this.props.IdCard);
+function Card(props) {
+    let card = Cards.filter(x=>x.id == props.IdCard)[0];
     
-  return (<h1></h1>);
+  return (
+  <div className={"card card-" + card.imageCode}>
+    <div className="card-top">
+      {
+        ((card) => {
+          if(card.victoryPoints > 0)
+            return (<div className="card-vp">{card.victoryPoints}</div>)
+        })(card)
+      }
+      <div className={"card-gem card-gem-" + card.gem}>{card.gem[0]}</div>
+    </div>
+    <div className="card-bottom">
+      {
+        card.cost.map(c=>{
+          return (<div className="card-cost"> {c.gem[0]} <span>{c.qty}</span> </div>)
+        })
+      }
+    </div>
+  </div>);
 }
 export default Card;

cfad693b0b93ee2a0552597cb755a6806da6d9e6
Becker Portilla <bportilla@baufest.com>
2020-02-12 10:31:06 -0500

    Init main page

---
+++ src/components/presentation/main/main.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Grid from '@material-ui/core/Grid/Grid'
+import { Container } from '@material-ui/core';
+
+function Main(props){
+    
+    return (
+        <Container maxWidth="sm">
+            
+        </Container>
+    );
+}
+
+export default Main;
